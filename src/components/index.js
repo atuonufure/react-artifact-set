548339cca818588475e143696a7986de08757e06
atuonufure <atuonufure@gmail.com>
2021-01-18 13:20:29 +0300

    save before rebuild

--- src/components/index.js
+++ src/components/index.js
@@ -13,38 +13,24 @@ const percent = (min, max) => {
 const Main = () => {
   const [flatAttack, setFlatAttack] = useState(flat(14, 19));
   const [percentAttack, setPercentAttack] = useState(percent(4.1, 5.8));
+  const [flatDefence, setFlatDefence] = useState(flat(16, 23));
+  const [percentDefence, setPercentDefence] = useState(flat(16, 23));
 
   return (
     <div>
-      <div>
-        <span>Сила атаки {flatAttack} </span>
-        <button onClick={flatAttack > 14 ? () => setFlatAttack(flatAttack - 1) : ''}>
-          уменьшить
-        </button>
-        <button onClick={flatAttack < 19 ? () => setFlatAttack(flatAttack + 1) : ''}>
-          прибавить
-        </button>
-      </div>
-      <div>
-        <span>Сила атаки {percentAttack}% </span>
-        <button
-          onClick={
-            percentAttack > 4.1 ? () => setPercentAttack(+(percentAttack - 0.1).toFixed(1)) : ''
-          }>
-          уменьшить
-        </button>
-        <button
-          onClick={
-            percentAttack < 5.8 ? () => setPercentAttack(+(percentAttack + 0.1).toFixed(1)) : ''
-          }>
-          прибавить
-        </button>
-      </div>
-      <div>
-        <button onClick={() => setFlatAttack(flat(14, 19)) & setPercentAttack(percent(4.1, 5.8))}>
-          обновить
-        </button>
-      </div>
+      <div>Сила атаки +{flatAttack} </div>
+      <div>Сила атаки +{percentAttack}% </div>
+      <div>Защита +{flatDefence} </div>
+      <div>Защита +{percentDefence}% </div>
+      <button
+        onClick={() =>
+          setFlatAttack(flat(14, 19)) &
+          setPercentAttack(percent(4.1, 5.8)) &
+          setFlatDefence(flat(16, 23)) &
+          setPercentDefence(percent(5.1, 7.3))
+        }>
+        обновить
+      </button>
     </div>
   );
 };
