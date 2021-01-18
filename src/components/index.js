7b412daa53be406628afb8b89a51394d72ebc18f
atuonufure <atuonufure@gmail.com>
2021-01-18 17:19:39 +0300

    first version of four random stats

--- src/components/index.js
+++ src/components/index.js
@@ -1,30 +1,19 @@
 import React, { useState } from 'react';
 
-const Array_flat_HP = [209, 239, 269, 299];
-const Array_flat_ATK = [14, 16, 18, 19];
-const Array_flat_DEF = [16, 19, 21, 23];
-const Array_percent_HP = [4.1, 4.7, 5.3, 5.8];
-const Array_percent_ATK = [4.1, 4.7, 5.3, 5.8];
-const Array_percent_DEF = [5.1, 5.8, 6.6, 7.3];
-const Array_ElementalMastery = [16, 19, 21, 23];
-const Array_EnergyRecharge = [4.5, 5.2, 5.8, 6.5];
-const Array_CRIT_Rate = [2.7, 3.1, 3.5, 3.9];
-const Array_CRIT_DMG = [5.4, 6.2, 7.0, 7.8];
-
 const calcAnyArray = (array) => array[Math.floor(Math.random() * array.length)];
 
-const Main = () => {
-  const random_flat_HP = () => calcAnyArray(Array_flat_HP);
-  const random_flat_ATK = () => calcAnyArray(Array_flat_ATK);
-  const random_flat_DEF = () => calcAnyArray(Array_flat_DEF);
-  const random_percent_HP = () => calcAnyArray(Array_percent_HP);
-  const random_percent_ATK = () => calcAnyArray(Array_percent_ATK);
-  const random_percent_DEF = () => calcAnyArray(Array_percent_DEF);
-  const random_ElementalMastery = () => calcAnyArray(Array_ElementalMastery);
-  const random_EnergyRecharge = () => calcAnyArray(Array_EnergyRecharge);
-  const random_CRIT_Rate = () => calcAnyArray(Array_CRIT_Rate);
-  const random_CRIT_DMG = () => calcAnyArray(Array_CRIT_DMG);
+const random_flat_HP = () => calcAnyArray([209, 239, 269, 299]);
+const random_flat_ATK = () => calcAnyArray([14, 16, 18, 19]);
+const random_flat_DEF = () => calcAnyArray([16, 19, 21, 23]);
+const random_percent_HP = () => calcAnyArray([4.1, 4.7, 5.3, 5.8]);
+const random_percent_ATK = () => calcAnyArray([4.1, 4.7, 5.3, 5.8]);
+const random_percent_DEF = () => calcAnyArray([5.1, 5.8, 6.6, 7.3]);
+const random_ElementalMastery = () => calcAnyArray([16, 19, 21, 23]);
+const random_EnergyRecharge = () => calcAnyArray([4.5, 5.2, 5.8, 6.5]);
+const random_CRIT_Rate = () => calcAnyArray([2.7, 3.1, 3.5, 3.9]);
+const random_CRIT_DMG = () => calcAnyArray([5.4, 6.2, 7.0, 7.8]);
 
+const Main = () => {
   const [flatHP, setFlatHP] = useState(random_flat_HP);
   const [flatATK, setFlatATK] = useState(random_flat_ATK);
   const [flatDEF, setFlatDEF] = useState(random_flat_DEF);
@@ -36,18 +25,73 @@ const Main = () => {
   const [critRate, setCritRate] = useState(random_CRIT_Rate);
   const [critDmg, setCritDmg] = useState(random_CRIT_DMG);
 
+  let arr = [];
+  while (arr.length < 4) {
+    let r = Math.floor(Math.random() * 10) + 1;
+    if (arr.indexOf(r) === -1) arr.push(r);
+  }
+
+  const FlatHP = () => {
+    return <div>HP +{flatHP} </div>;
+  };
+  const FlatATK = () => {
+    return <div>ATK +{flatATK} </div>;
+  };
+  const FlatDEF = () => {
+    return <div>DEF +{flatDEF} </div>;
+  };
+  const FlatPercentHP = () => {
+    return <div>HP +{flatPercentHP}% </div>;
+  };
+  const FlatPercentATK = () => {
+    return <div>ATK +{flatPercentATK}% </div>;
+  };
+  const FlatPercentDEF = () => {
+    return <div>DEF +{flatPercentDEF}% </div>;
+  };
+  const ElementalMastery = () => {
+    return <div>Elemental Mastery +{elementalMastery} </div>;
+  };
+  const EnergyRecharge = () => {
+    return <div>Energy Recharge +{energyRecharge}% </div>;
+  };
+  const CritRate = () => {
+    return <div>CRIT Rate +{critRate}% </div>;
+  };
+  const CritDmg = () => {
+    return <div>CRIT DMG +{critDmg}% </div>;
+  };
+
+  let stats = arr.map(function (item, index, array) {
+    switch (item) {
+      case 1:
+        return <FlatHP />;
+      case 2:
+        return <FlatATK />;
+      case 3:
+        return <FlatDEF />;
+      case 4:
+        return <FlatPercentHP />;
+      case 5:
+        return <FlatPercentATK />;
+      case 6:
+        return <FlatPercentDEF />;
+      case 7:
+        return <ElementalMastery />;
+      case 8:
+        return <EnergyRecharge />;
+      case 9:
+        return <CritRate />;
+      case 10:
+        return <CritDmg />;
+      default:
+        return alert('fail');
+    }
+  });
+
   return (
     <div>
-      <div>HP +{flatHP} </div>
-      <div>ATK +{flatATK} </div>
-      <div>DEF +{flatDEF} </div>
-      <div>HP +{flatPercentHP}% </div>
-      <div>ATK +{flatPercentATK}% </div>
-      <div>DEF +{flatPercentDEF}% </div>
-      <div>Elemental Mastery +{elementalMastery} </div>
-      <div>Energy Recharge +{energyRecharge}% </div>
-      <div>CRIT Rate +{critRate}% </div>
-      <div>CRIT DMG +{critDmg}% </div>
+      {stats}
       <button
         onClick={() =>
           setFlatHP(random_flat_HP) &
@@ -59,21 +103,9 @@ const Main = () => {
           setElementalMastery(random_ElementalMastery) &
           setEnergyRecharge(random_EnergyRecharge) &
           setCritRate(random_CRIT_Rate) &
-          setCritDmg(random_CRIT_DMG) &
-          console.log(
-            `flatHP = ${flatHP}`,
-            `flatATK = ${flatATK}`,
-            `flatDEF = ${flatDEF}`,
-            `flatPercentHP = ${flatPercentHP}`,
-            `flatPercentATK = ${flatPercentATK}`,
-            `flatPercentDEF = ${flatPercentDEF}`,
-            `elementalMastery = ${elementalMastery}`,
-            `energyRecharge = ${energyRecharge}`,
-            `critRate = ${critRate}`,
-            `critDmg = ${critDmg}`,
-          )
+          setCritDmg(random_CRIT_DMG)
         }>
-        обновить
+        refresh
       </button>
     </div>
   );
